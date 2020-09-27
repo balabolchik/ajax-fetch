3448fc8a78baff7f5b2f76c71f2061eba232d1b9
balabolchik <obordulyak@gmail.com>
2020-09-27 10:58:57 +0300

    fetch

--- js/index.js
+++ js/index.js
@@ -12,3 +12,13 @@ function getHtmlAjax() {
   xhr.open('GET', 'client-data.html', true);
   xhr.send();
 }
+
+document.querySelector('.fetch').addEventListener('click', fetchHtml);
+
+async function fetchHtml () {
+    // fetch('client-data.html')
+    // .then( Response => Response.text() )
+    // .then ( html => document.querySelector('.html-container').innerHTML = html);
+    const response = await fetch('client-data.html');
+    document.querySelector('.html-container').innerHTML = await response.text();
+}
